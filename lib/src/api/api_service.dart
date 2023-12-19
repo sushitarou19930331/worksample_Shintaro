2217ed1d82a06002082b14255a3d8ff1a2285940
Shintaro Sato <123647593+sushitarou19930331@users.noreply.github.com>
2023-12-19 21:38:00 +0100

    Update api_service.dart

--- lib/src/api/api_service.dart
+++ lib/src/api/api_service.dart
@@ -1,13 +1,20 @@
+import 'package:http/http.dart' as http;
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:worksample/src/users/user.dart';
 
 final apiServiceProvider = Provider<ApiService>((ref) => ApiService());
 
 class ApiService {
-  // ToDo: Replace this with a call to the users API https://dummyjson.com/docs/users
-  Future<List<User>> getUsers() async {
-    await Future.delayed(const Duration(seconds: 1));
-    return List.generate(10, (i) => User(i));
+   Future<List<User>> fetchUsers() async {
+    var client = http.Client();
+
+    var uri = Uri.parse('https://dummyjson.com/docs/users');
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return List.generate(10, (i) => User(i));
+    }
+    return [ ];
   }
-}
-///
+ }
